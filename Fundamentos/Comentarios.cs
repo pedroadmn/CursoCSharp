a0dc82f150de59ec999463abe885369ecfcfe233
Pedro Adelino <pedro.mello@virtus.ufcg.edu.br>
2019-04-03 07:38:53 -0300

    Initializing C Sharp Course by Cod3r

---
+++ Fundamentos/Comentarios.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CursoCSharp.Fundamentos
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    class Comentarios
+    {
+        public static void Executar()
+        {
+            // Cuidado com os comentários desnecessários
+            Console.WriteLine("Código claro é sempre melhor");
+
+            /*
+             *
+             */
+            Console.WriteLine("O C# tem o XML Comments.");
+        }
+    }
+}
