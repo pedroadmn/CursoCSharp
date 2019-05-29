13d00689b070012a2f29b64d48193d75c051a148
Pedro Adelino <pedro.mello@virtus.ufcg.edu.br>
2019-05-29 08:03:25 -0300

    Adicionando fundamentos sobre Notação Ponto

--- Fundamentos/NotacaoPonto.cs
+++ Fundamentos/NotacaoPonto.cs
@@ -8,7 +8,13 @@ namespace CursoCSharp.Fundamentos
     {
         public static void Executar()
         {
+            var saudacao = "olá".ToUpper().Insert(3, " World!".Replace("World!", "Mundo!"));
+            Console.WriteLine(saudacao);
 
+            Console.WriteLine("Teste".Length);
+
+            string valorImportante = null;
+            Console.WriteLine(valorImportante?.Length);
         }
     }
 }
